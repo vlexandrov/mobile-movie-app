5622180e0b301d583baccb24e360455febc682c4
Vlex Androv Ponsaran <101042522+vlexandrov@users.noreply.github.com>
2025-09-20 15:34:18 +1200

    styling setup: tailwind setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
+//import "@/global.css";
 import { Text, View } from "react-native";
+
+// export default function Index() {
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <Text>Edit app/index.tsx to edit this screen.</Text>
+//     </View>
+//   );
+// }
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
     </View>
   );
 }
