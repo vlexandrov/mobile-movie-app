a1fadf9030c53815ed14f5855c4ea704f71ff5c3
Vlex Androv Ponsaran <101042522+vlexandrov@users.noreply.github.com>
2025-09-20 16:03:20 +1200

    routing/nav: update index.tsx w/ links to new pages

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 //import "@/global.css";
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 // export default function Index() {
@@ -19,6 +20,8 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl font-bold text-dark-100">Welcome!</Text>
+      <Link href="/onboarding">Onboarding</Link>
+      <Link href="/movie/avengers">Avenger Movie</Link>
     </View>
   );
 }
