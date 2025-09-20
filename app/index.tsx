9b7dceaf70a92097a73e2e36f912afc1df3af632
Vlex Androv Ponsaran <101042522+vlexandrov@users.noreply.github.com>
2025-09-20 15:52:29 +1200

    styling setup: extend Tailwind CSS color palette

--- app/index.tsx
+++ app/index.tsx
@@ -18,9 +18,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-5xl font-bold text-dark-100">Welcome!</Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,22 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: "D6C6FF",
+          200: "#A8B5DB",
+          300: "#9CA4AB",
+        },
+        dark: {
+          100: "#221F3D",
+          200: "0F0D23",
+        },
+        accent: "#AB8BFF",
+      },
+    },
   },
   plugins: [],
 };
