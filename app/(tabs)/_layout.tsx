00b7aedde6115c17590dd28e286b15673d9a9841
Vlex Androv Ponsaran <101042522+vlexandrov@users.noreply.github.com>
2025-09-20 16:22:49 +1200

    routing/layout: update RootLayout to include tabs navigation

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,37 @@
+import { Tabs } from "expo-router";
+import React from "react";
+
+export default function _layout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "Saved",
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+        }}
+      />
+    </Tabs>
+  );
+}
