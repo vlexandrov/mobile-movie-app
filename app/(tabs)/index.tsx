1884a57caacc25450360d422e269b6ba5668c165
Vlex Androv Ponsaran <101042522+vlexandrov@users.noreply.github.com>
2025-09-23 07:41:14 +1200

    Home Screen UI: CREATE COMPONENT

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,8 @@
 //import "@/global.css";
+import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
 import { Image, ScrollView, View } from "react-native";
 
 // export default function Index() {
@@ -18,6 +20,8 @@ import { Image, ScrollView, View } from "react-native";
 // }
 
 export default function Index() {
+  const router = useRouter();
+
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
@@ -30,6 +34,12 @@ export default function Index() {
         }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+        <View className="flex-1 mt-5">
+          <SearchBar
+            onPress={() => router.push("/search")}
+            placeholder="Search for a movie"
+          />
+        </View>
       </ScrollView>
     </View>
   );
