0297f228462fd2e12b79e899e302a2471e19df93
Vlex Androv Ponsaran <101042522+vlexandrov@users.noreply.github.com>
2025-09-23 07:18:51 +1200

    Home Screen UI: style bg and icon on home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 //import "@/global.css";
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
 
 // export default function Index() {
 //   return (
@@ -18,10 +19,18 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl font-bold text-dark-100">Welcome!</Text>
-      <Link href="/onboarding">Onboarding</Link>
-      <Link href="/movie/avengers">Avenger Movie</Link>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView
+        className="flex-1 px-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+          minHeight: "100%",
+          paddingBottom: 10,
+        }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+      </ScrollView>
     </View>
   );
 }
