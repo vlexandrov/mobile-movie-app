feb9d1e37259d766188f18fdd6095dac625988e1
Vlex Androv Ponsaran <101042522+vlexandrov@users.noreply.github.com>
2025-09-23 20:52:01 +1200

    custom hook: useFetch hook

---
+++ services/useFetch.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const result = await fetchFunction();
+
+      setData(result);
+    } catch (err) {
+      setError(err instanceof Error ? err : new Error("An error occured"));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const reset = () => {
+    setData(null);
+    setLoading(false);
+    setError(null);
+  };
+
+  // called when you want to do something at start of component load
+  // check if autofetch is turned on; if true, call fetchData function
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData;
+    }
+  }, []);
+
+  return { data, loading, error, refetch: fetchData, reset };
+};
+
+export default useFetch;
+
+// useFetch is a custom hook that abstracts the logic for fetching data from an API.
+// It manages loading, error, and data states, and provides a refetch function to manually trigger data fetching.
