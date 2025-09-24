33181286e7a176d9f83d516a7b4f80c187955518
Vlex Androv Ponsaran <101042522+vlexandrov@users.noreply.github.com>
2025-09-25 02:49:14 +1200

    fix: add resizeMode to background image for better display

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -30,7 +30,11 @@ export default function Index() {
 
   return (
     <View className="flex-1 bg-primary">
-      <Image source={images.bg} className="absolute w-full z-0" />
+      <Image
+        source={images.bg}
+        className="absolute w-full z-0"
+        resizeMode="cover"
+      />
       <ScrollView
         className="flex-1 px-5"
         showsHorizontalScrollIndicator={false}
