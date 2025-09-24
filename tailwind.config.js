3bb0ea94e25730cf17d8c1e4bfa42a5ae3813727
Vlex Androv Ponsaran <101042522+vlexandrov@users.noreply.github.com>
2025-09-25 03:49:23 +1200

    fix: fix color values in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,13 +13,13 @@ module.exports = {
         primary: "#030014",
         secondary: "#151312",
         light: {
-          100: "D6C6FF",
+          100: "#D6C6FF",
           200: "#A8B5DB",
           300: "#9CA4AB",
         },
         dark: {
           100: "#221F3D",
-          200: "0F0D23",
+          200: "#0F0D23",
         },
         accent: "#AB8BFF",
       },
