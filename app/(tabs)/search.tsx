d8f73433189495f298217bbe7969f0652ba3c037
Vlex Androv Ponsaran <101042522+vlexandrov@users.noreply.github.com>
2025-09-25 04:15:29 +1200

    search screen: initial styling

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,90 @@
+import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import React from "react";
-import { Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 
 export default function Search() {
+  // search query state
+
+  // function to handle search queries
+
+  // fetch movies based on search query
+  const {
+    data: movies,
+    loading,
+    error,
+  } = useFetch(() =>
+    fetchMovies({
+      query: "",
+    })
+  );
+
+  // debounce search
+
+  //render
   return (
-    <View>
-      <Text>S</Text>
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      />
+
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        className="px-5"
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "center",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center items-center">
+              <Image
+                source={icons.logo}
+                className="w-12 h-10 mt-20 mb-5 mx-auto"
+              />
+            </View>
+
+            <View className="my-5">
+              <SearchBar placeholder="Search movies..." />
+            </View>
+
+            {loading && (
+              <ActivityIndicator
+                size="large"
+                color="#0000ff"
+                className="my-3"
+              />
+            )}
+
+            {error && (
+              <Text className="text-red-500 px-5 my-3">
+                Error: {error.message}
+              </Text>
+            )}
+
+            {!loading &&
+              !error &&
+              "SEARCH TERM".trim() &&
+              movies?.length > 0 && (
+                <Text className="text-xl text-white">
+                  Search results for{" "}
+                  <Text className="text-accent">SEARCH TERM</Text>
+                </Text>
+              )}
+          </>
+        }
+      />
     </View>
   );
 }
