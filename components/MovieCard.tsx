94306a335e0abeb4b97f352fb985c1b9608befbf
Vlex Androv Ponsaran <101042522+vlexandrov@users.noreply.github.com>
2025-09-25 22:59:42 +1200

    fix(MovieCard): update vote_average display to one decimal place

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -32,7 +32,7 @@ export default function MovieCard({
         <View className="flex-row items-center justify-start gap-x-1">
           <Image source={icons.star} className="size-4" />
           <Text className="text-white text-xs font-bold uppercase">
-            {vote_average.toFixed(2)}
+            {vote_average.toFixed(1)}
           </Text>
         </View>
 
