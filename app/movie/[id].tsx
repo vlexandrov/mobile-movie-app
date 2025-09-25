01012dad2f1f3d1dce9687949f6727334ea74806
Vlex Androv Ponsaran <101042522+vlexandrov@users.noreply.github.com>
2025-09-26 01:32:47 +1200

    movie details: create page for movie details

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -1,9 +1,10 @@
+import MovieInfo from "@/components/MovieInfo";
 import { icons } from "@/constants/icons";
 import { fetchMovieDetails } from "@/services/api";
 import useFetch from "@/services/useFetch";
-import { useLocalSearchParams } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import React from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 
 export default function MovieDetails() {
   const { id } = useLocalSearchParams();
@@ -20,34 +21,92 @@ export default function MovieDetails() {
             source={{
               uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`,
             }}
-            className="w-full h-[550px]"
+            className="w-full h-[650px]"
             resizeMode="stretch"
           />
 
+          {/* movie data section */}
           <View className="flex-col items-start justify-center mt-5 px-5">
+            {/* title */}
             <Text className="text-white font-bold text-xl">{movie?.title}</Text>
 
+            {/* year + runtime */}
             <View className="flex-row items-center gap-x-1 mt-2">
               <Text className="text-light-200 text-sm">
                 {movie?.release_date?.split("-")[0]}
               </Text>
-              <Text className="text-light-200 text-sm">{movie?.runtime}m</Text>
+              <Text className="text-light-200 text-sm">
+                | {movie?.runtime}m
+              </Text>
             </View>
 
+            {/* votes */}
             <View className="flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2">
               <Image source={icons.star} className="size-4" />
+
               <Text className="text-white font-bold text-sm">
                 {movie?.vote_average
                   ? movie.vote_average.toFixed(1)
                   : "No ratings"}
               </Text>
+
               <Text className="text-light-200 text-sm">
                 ({movie?.vote_count} votes)
               </Text>
             </View>
           </View>
+          {/* end of movie data section */}
+
+          {/* more details section */}
+          <MovieInfo label="Overview" value={movie?.overview} />
+
+          <MovieInfo
+            label="Genres"
+            value={movie?.genres?.map((g) => g.name).join(" • ") || "N/A"}
+          />
+
+          <View className="flex flex-row justify-between w-1/2">
+            <MovieInfo
+              label="Budget"
+              // value={`$${(movie?.budget ?? 0) / 1000000} million` || "Unknown"}
+              value={
+                movie?.budget
+                  ? `$${movie?.budget / 1000000} million`
+                  : "Unknown"
+              }
+            />
+            <MovieInfo
+              label="Revenue"
+              value={
+                movie?.revenue
+                  ? `$${(movie?.revenue / 1000000).toFixed(1)} million`
+                  : "Unknown"
+              }
+            />
+          </View>
+
+          <MovieInfo
+            label="Production Companies"
+            value={
+              movie?.production_companies.map((p) => p.name).join(" • ") ||
+              "Unknown"
+            }
+          />
+          {/* end of more details section */}
         </View>
       </ScrollView>
+
+      <TouchableOpacity
+        className="absolute bottom-5 left-0 right-0 mx-5 bg-accent rounded-lg flex-row justify-center items-center z-50 py-3.5"
+        onPress={router.back}
+      >
+        <Image
+          source={icons.arrow}
+          className="size-5 mr-1 mt-0.5 rotate-180"
+          tintColor={"#fff"}
+        />
+        <Text className="text-white font-semibold text-base">Go Back</Text>
+      </TouchableOpacity>
     </View>
   );
 }
