92fe6aa0e52eb29f4ee37464e8d84186e8587adb
Vlex Androv Ponsaran <101042522+vlexandrov@users.noreply.github.com>
2025-09-25 17:00:44 +1200

    search screen: add ListEmptyComponent to display messages when no movies are found

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -104,6 +104,17 @@ export default function Search() {
             )}
           </>
         }
+        ListEmptyComponent={
+          !loading && !error ? (
+            <View className="mt-10 px-5">
+              <Text className="text-center text-gray-500 text-lg">
+                {searchQuery.trim()
+                  ? "No movies found :("
+                  : "Start typing to search for movies"}
+              </Text>
+            </View>
+          ) : null
+        }
       />
     </View>
   );
